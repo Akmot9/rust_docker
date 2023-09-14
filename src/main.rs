1d742705e12075ef28df94ccef4d0e5e1be0b8a7
akmot9 <avicocyprien@yahoo.com>
2023-09-14 13:29:21 +0200

    2 f

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 fn main() {
     println!("Hello, world! ouiii");
+    println!("5 + 3 = {}", addition(5, 3));
+    println!("5 - 3 = {}", subtraction(5, 3));
+}
 
+fn addition(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn subtraction(a: i32, b: i32) -> i32 {
+    a - b
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::main;
+    use super::*;
 
-    
     #[test]
-    fn test_main() {
-        // Votre logique de test ici.
-        assert_eq!(1, 1);  // Cette assertion vérifie si 1 est égal à 1
-        main();
+    fn test_addition() {
+        assert_eq!(addition(1, 1), 2);
+    }
+
+    #[test]
+    fn test_subtraction() {
+        assert_eq!(subtraction(5, 3), 2);
     }
 }
