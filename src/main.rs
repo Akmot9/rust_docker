f5d0f5e91b21e5c9342803fcc06e2d8e3500678d
akmot9 <avicocyprien@yahoo.com>
2023-09-14 09:26:51 +0200

    test unitaires

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world! ouiii");
 }
+
+#[cfg(test)]
+mod tests {
+    
+    #[test]
+    fn test_main() {
+        // Votre logique de test ici.
+        assert_eq!(1, 1);  // Cette assertion vérifie si 1 est égal à 1
+    }
+}
