cba3c89ba8eaf0966ffe1b797aaab10a5824aa20
akmot9 <avicocyprien@yahoo.com>
2023-09-14 14:41:12 +0200

    trivy

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use serde::{self, Serialize, Deserialize};
+use std::io::{self, Read};
+use libflate::gzip::Decoder;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+
 fn main() {
+    let encoded_data = [31, 139, 8, 0, 123, 0, 0, 0, 0, 3, 1, 12, 0, 243, 255,
+    72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33,
+    163, 28, 41, 28, 12, 0, 0, 0];
+
+    let mut decoder = Decoder::new(&encoded_data[..]).unwrap();
+    let mut buf: Vec<u8> = Vec::new();
+    decoder.read_to_end(&mut buf).unwrap();
+
+    assert_eq!(buf, b"Hello World!");
+
     println!("Hello, world! ouiii");
     println!("5 + 3 = {}", addition(5, 3));
     println!("5 - 3 = {}", subtraction(5, 3));
