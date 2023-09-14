618a51c7b5eb285628851955b5c1a9d8d0f02b2b
akmot9 <avicocyprien@yahoo.com>
2023-09-14 13:22:18 +0200

    test main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
     println!("Hello, world! ouiii");
+
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::main;
+
     
     #[test]
     fn test_main() {
         // Votre logique de test ici.
         assert_eq!(1, 1);  // Cette assertion vérifie si 1 est égal à 1
+        main();
     }
 }
