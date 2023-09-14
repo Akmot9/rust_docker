98d6adbe74edecd87f14c40695cfd1c80a37f579
akmot9 <avicocyprien@yahoo.com>
2023-09-14 15:02:16 +0200

    fix trivy k

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde::{self, Serialize, Deserialize};
-use std::io::{self, Read};
-use libflate::gzip::Decoder;
+use std::io::{self, Read, Write};
+use libflate::gzip::{Encoder, Decoder};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Point {
@@ -9,22 +9,49 @@ struct Point {
     c: i32,
 }
 
+fn compress(data: &[u8]) -> io::Result<Vec<u8>> {
+    let mut encoder = Encoder::new(Vec::new())?;
+    encoder.write_all(data)?;
+    let compressed_data = encoder.finish().into_result()?;
+    Ok(compressed_data)
+}
+
+fn decompress(encoded_data: &[u8]) -> io::Result<Vec<u8>> {
+    let mut decoder = Decoder::new(encoded_data)?;
+    let mut buf: Vec<u8> = Vec::new();
+    decoder.read_to_end(&mut buf)?;
+    Ok(buf)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compression_decompression() {
+        let original_data = b"Hello, world!";
+        let compressed_data = compress(original_data).unwrap();
+        let decompressed_data = decompress(&compressed_data).unwrap();
+
+        assert_eq!(original_data.to_vec(), decompressed_data);
+    }
+
+    #[test]
+    fn test_addition() {
+        assert_eq!(addition(1, 1), 2);
+    }
+
+    #[test]
+    fn test_subtraction() {
+        assert_eq!(subtraction(5, 3), 2);
+    }
+}
 
 fn main() {
-    let encoded_data = [31, 139, 8, 0, 123, 0, 0, 0, 0, 3, 1, 12, 0, 243, 255,
-    72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33,
-    163, 28, 41, 28, 12, 0, 0, 0];
-
-    let mut decoder = Decoder::new(&encoded_data[..]).unwrap();
-    let mut buf: Vec<u8> = Vec::new();
-    decoder.read_to_end(&mut buf).unwrap();
-
-    assert_eq!(buf, b"Hello World!");
-
     println!("Hello, world! ouiii");
     println!("5 + 3 = {}", addition(5, 3));
     println!("5 - 3 = {}", subtraction(5, 3));
-    division(2,2);
+    println!("2 / 2 = {}", division(2, 2));
 }
 
 fn addition(a: i32, b: i32) -> i32 {
@@ -36,20 +63,8 @@ fn subtraction(a: i32, b: i32) -> i32 {
 }
 
 fn division(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        return 0;
+    }
     a / b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_addition() {
-        assert_eq!(addition(1, 1), 2);
-    }
-
-    #[test]
-    fn test_subtraction() {
-        assert_eq!(subtraction(5, 3), 2);
-    }
 }
